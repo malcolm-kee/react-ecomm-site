c83c89a49e73fe41e5344cca81c45fc97c29d631
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-03-08 14:41:33 +0800

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-testing-library';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  });
 });
