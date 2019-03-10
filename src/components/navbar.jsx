c0ed2a99e3001fbec9d848e97d91f5f53d676f77
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-03-10 16:02:53 +0800

    fix navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,18 +4,16 @@ import { Link } from '@reach/router';
 export function Navbar() {
   return (
     <nav className="navbar navbar-default">
-      <div className="container-fluid">
+      <div className="container">
         <div className="navbar-header">
           <Link className="navbar-brand" to="/">
             Shopit
           </Link>
-        </div>
-        <div className="collapse navbar-collapse">
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-          </ul>
+          <div className="navbar-text">
+            <Link to="/profile" className="navbar-link">
+              Profile
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
