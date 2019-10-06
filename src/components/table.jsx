48f1a8673c186cf4506ec34ed702580e23940130
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-06 19:36:17 +0800

    add table component

---
+++ src/components/table.jsx
@@ -0,0 +1,44 @@
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+/**
+ * @see https://getbootstrap.com/docs/3.3/css/#tables
+ */
+export const Table = ({
+  bordered,
+  condensed,
+  hover,
+  responsive,
+  striped,
+  className,
+  ...tableProps
+}) => {
+  const tableNode = (
+    <table
+      className={cx(
+        'table',
+        striped && 'table-striped',
+        bordered && 'table-bordered',
+        hover && 'table-hover',
+        condensed && 'table-condensed',
+        className
+      )}
+      {...tableProps}
+    />
+  );
+
+  return responsive ? (
+    <div className="table-responsive">{tableNode}</div>
+  ) : (
+    tableNode
+  );
+};
+
+Table.propTypes = {
+  bordered: PropTypes.bool,
+  condensed: PropTypes.bool,
+  hover: PropTypes.bool,
+  responsive: PropTypes.bool,
+  striped: PropTypes.bool
+};
