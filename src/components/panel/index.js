a2fab502c54e8d1fc2648f4ddc7c2b37d5b1ec30
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-06 14:24:32 +0800

    add panel component

---
+++ src/components/panel/index.js
@@ -0,0 +1,4 @@
+export { Panel } from './panel';
+export { PanelBody } from './panel-body';
+export { PanelFooter } from './panel-footer';
+export { PanelHeading } from './panel-heading';
---
+++ src/components/panel/panel-heading.jsx
@@ -0,0 +1,11 @@
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export const PanelHeading = ({ children, className }) => {
+  return <div className={cx('panel-heading', className)}>{children}</div>;
+};
+
+PanelHeading.propTypes = {
+  className: PropTypes.string
+};
