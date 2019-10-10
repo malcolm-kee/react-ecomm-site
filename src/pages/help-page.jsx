74fda20f4fe269d148ed427552e73fa3c2a398a3
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-10 22:09:46 +0800

    complete annoying feedback

--- src/pages/help-page.jsx
+++ src/pages/help-page.jsx
@@ -8,6 +8,10 @@ import { ListGroup } from '../components/list-group';
 import { Panel, PanelBody } from '../components/panel';
 
 const FeedbackPanel = () => {
+  const [hovered, setHovered] = React.useState(false);
+  const [focused, setFocused] = React.useState(false);
+  const inverted = hovered || focused;
+
   const onFeedback = () => {
     toast('Thanks for your feedback! But we will not record it anywhere.', {
       type: 'success',
@@ -20,16 +24,23 @@ const FeedbackPanel = () => {
       <PanelBody>
         <p>Was this article helpful?</p>
         <div className="btn-toolbar">
-          <Button onClick={onFeedback} color="primary">
-            Yes{' '}
+          <Button onClick={onFeedback} color={inverted ? 'danger' : 'success'}>
+            {inverted ? 'No ' : 'Yes '}
             <span role="img" aria-hidden>
-              👍
+              {inverted ? '👎' : '👍'}
             </span>
           </Button>
-          <Button disabled>
-            No
+          <Button
+            onClick={onFeedback}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+            color={inverted ? 'success' : 'danger'}
+          >
+            {inverted ? 'Yes ' : 'No '}
             <span role="img" aria-hidden>
-              👎
+              {inverted ? '👍' : '👎'}
             </span>
           </Button>
         </div>
