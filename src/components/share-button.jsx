bd9371d8009e4f4ae00668a2de0a454abb487ca6
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-19 17:29:56 +0800

    add share button

---
+++ src/components/share-button.jsx
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useTransientState } from '../hooks/use-transient-state';
+import { copyText } from '../lib/copy';
+import { isShareSupported, share } from '../lib/share';
+import { Button } from './button';
+
+/**
+ * A button to share an URL using Web Share API.
+ * Fallback to copy to clipboard when Web Share API is not supported.
+ */
+export const ShareButton = ({
+  urlToShare,
+  color = 'primary',
+  label = 'Share',
+  titleToShare = ''
+}) => {
+  const [copied, setCopied] = useTransientState(false);
+
+  const handleClick = () => {
+    if (isShareSupported()) {
+      share({
+        url: urlToShare,
+        title: titleToShare
+      });
+    } else {
+      copyText(urlToShare);
+      setCopied(true);
+    }
+  };
+
+  return (
+    <Button onClick={handleClick} color={copied ? 'success' : color}>
+      {copied ? 'Link copied!' : label}
+    </Button>
+  );
+};
+
+ShareButton.propTypes = {
+  urlToShare: PropTypes.string.isRequired,
+  color: PropTypes.oneOf([
+    'default',
+    'primary',
+    'success',
+    'info',
+    'warning',
+    'danger',
+    'link'
+  ]),
+  label: PropTypes.node,
+  /** title of the Share Popup. Note that this has no effect when we fallback to copy */
+  titleToShare: PropTypes.string
+};
---
+++ src/hooks/use-transient-state.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const useTransientState = (steadyState, restorationTime = 1000) => {
+  const [state, setState] = React.useState(steadyState);
+  const [calledTimes, setCallTimes] = React.useState(0);
+
+  const setTemporaryState = React.useCallback(function setTemporaryState(
+    newValue
+  ) {
+    setState(newValue);
+    setCallTimes(t => t + 1);
+  },
+  []);
+
+  React.useEffect(() => {
+    if (state !== steadyState && restorationTime) {
+      const timeoutId = setTimeout(
+        () => setState(steadyState),
+        restorationTime
+      );
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [state, steadyState, restorationTime, calledTimes]);
+
+  return [state, setTemporaryState];
+};
