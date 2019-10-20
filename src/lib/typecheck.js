dad10f437a399cae2c56aaaed2cff30b2c3bf81d
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-20 13:24:34 +0800

    fix radio group and add tests

--- src/lib/typecheck.js
+++ src/lib/typecheck.js
@@ -1,3 +1,5 @@
+export const isDefined = value => typeof value !== 'undefined';
+
 export const isBoolean = value => typeof value === 'boolean';
 
-export const isPrimitive = value => /^(b|s|n)/.test(typeof value);
+export const isPrimitive = value => /^(b|st|n)/.test(typeof value);
