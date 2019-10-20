57204c822c012cbca3eb484dc664c5640934eea4
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-20 15:22:32 +0800

    add tests

---
+++ src/components/list-group.spec.jsx
@@ -0,0 +1,44 @@
+import { fireEvent, render } from '@testing-library/react';
+import * as React from 'react';
+import { ListGroup } from './list-group';
+
+describe('ListGroup button variant', () => {
+  test('all button props will be passed on', () => {
+    const callBack = jest.fn();
+    const onFocus = jest.fn();
+    const buttonRef = React.createRef();
+
+    const { getByText } = render(
+      <ListGroup
+        variant="button"
+        items={[
+          {
+            label: 'Malcolm',
+            onClick: () => callBack('malcolm'),
+            variant: 'success',
+            onFocus: onFocus,
+            ref: buttonRef
+          },
+          {
+            label: 'Hello',
+            onClick: () => callBack('hello'),
+            variant: 'warning',
+            disabled: true
+          }
+        ]}
+      />
+    );
+
+    fireEvent.click(getByText('Malcolm'));
+
+    expect(callBack).toHaveBeenCalledTimes(1);
+    expect(callBack).toHaveBeenLastCalledWith('malcolm');
+
+    fireEvent.click(getByText('Hello'));
+    // clicking disabled button has no effect
+    expect(callBack).toHaveBeenCalledTimes(1);
+
+    buttonRef.current.focus();
+    expect(onFocus).toHaveBeenCalledTimes(1);
+  });
+});
