dc0f36a1c7ede48b398e4e9c84bc6b450b812718
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-23 02:55:46 +0800

    add select field

---
+++ src/components/select-field.spec.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { SelectField } from './select-field';
+
+describe('SelectField', () => {
+  it('can renders when options are passed as props', () => {
+    const onChangeValue = jest.fn();
+
+    const { getByLabelText } = render(
+      <SelectField
+        label="Lucky Number"
+        options={[
+          { label: 'One', value: '1' },
+          { label: 'Two', value: '2' },
+          { label: 'Three', value: '3' }
+        ]}
+        onChangeValue={onChangeValue}
+      />
+    );
+
+    fireEvent.change(getByLabelText('Lucky Number'), {
+      target: {
+        value: '2'
+      }
+    });
+
+    expect(onChangeValue).toHaveBeenCalledTimes(1);
+    expect(onChangeValue).toHaveBeenCalledWith('2');
+  });
+
+  it('can renders when options are passed as children', () => {
+    const onChangeValue = jest.fn();
+
+    const { getByLabelText } = render(
+      <SelectField label="Lucky Number" onChangeValue={onChangeValue}>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </SelectField>
+    );
+
+    fireEvent.change(getByLabelText('Lucky Number'), {
+      target: {
+        value: '2'
+      }
+    });
+
+    expect(onChangeValue).toHaveBeenCalledTimes(1);
+    expect(onChangeValue).toHaveBeenCalledWith('2');
+  });
+
+  it('forward all props supported by select', () => {
+    const selectRef = React.createRef();
+    const onFocus = jest.fn();
+
+    render(
+      <SelectField
+        label="Hey"
+        onFocus={onFocus}
+        options={[
+          { label: 'HTML', value: 'html' },
+          { label: 'CSS', value: 'css' },
+          { label: 'JavaScript', value: 'js' }
+        ]}
+        ref={selectRef}
+      />
+    );
+
+    selectRef.current.focus();
+
+    expect(onFocus).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/components/select.jsx
@@ -0,0 +1,78 @@
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { callAll } from '../lib/fn-lib';
+import { FieldContext } from './field-context';
+
+/**
+ * `Select` is a wrapper around `select` element.
+ *
+ * It accepts all props an `select` element in addition of the stated props.
+ *
+ * `ref` will be forwarded to the underlying `select` element.
+ *
+ * Most of the time you want to wrap this within `Field` component so it will provides
+ * contextual styling and good defaults on props
+ */
+export const Select = React.forwardRef(function Select(
+  {
+    size,
+    onChangeValue,
+    onChange,
+    options,
+    className,
+    children,
+    required,
+    ...selectProps
+  },
+  ref
+) {
+  const { inputId, setInputId } = React.useContext(FieldContext);
+
+  React.useEffect(() => {
+    if (selectProps.id && selectProps.id !== inputId) {
+      setInputId(selectProps.id);
+    }
+  }, [selectProps.id, setInputId, inputId]);
+
+  return (
+    <select
+      className={cx('form-control', size && `input-${size}`, className)}
+      id={inputId}
+      required={required}
+      onChange={callAll(
+        onChange,
+        onChangeValue && (ev => onChangeValue(ev.target.value))
+      )}
+      {...selectProps}
+      ref={ref}
+    >
+      {!selectProps.multiple && <option value=""></option>}
+      {options &&
+        options.map((option, index) => (
+          <option value={option.value} key={index}>
+            {option.label}
+          </option>
+        ))}
+      {children}
+    </select>
+  );
+});
+
+Select.propTypes = {
+  /**
+   * callback to be invoked when input change. The parameter will
+   * be the value instead of the event object
+   */
+  onChangeValue: PropTypes.func,
+  /**
+   * Helper props to generate the options. Alternatively you can construct the option markdown as children.
+   */
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired
+    })
+  ),
+  size: PropTypes.oneOf(['sm', 'lg'])
+};
