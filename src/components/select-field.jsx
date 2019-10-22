efe91fbef8eded47ecc41ab05559741ec76c8d21
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-23 02:55:46 +0800

    add select field

---
+++ src/components/select-field.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Field } from './field';
+import { HelpText } from './help-text';
+import { Label } from './label';
+import { FieldStatus } from './prop-types';
+import { Select } from './select';
+
+/**
+ * SelectField is just a composition of `Field`, `Select`, `Label`, and `HelpText`.
+ *
+ * Props not specified will be spreaded to `Select` element, including `ref`.
+ *
+ * If you need more control, compose them yourself.
+ */
+export const SelectField = React.forwardRef(function SelectField(
+  { status, helpText, label, ...selectProps },
+  ref
+) {
+  return (
+    <Field status={status}>
+      {label && <Label>{label}</Label>}
+      <Select {...selectProps} ref={ref} />
+      {helpText && <HelpText>{helpText}</HelpText>}
+    </Field>
+  );
+});
+
+SelectField.propTypes = {
+  status: FieldStatus,
+  helpText: PropTypes.node,
+  label: PropTypes.node
+};
---
+++ src/components/select-field.md
@@ -0,0 +1,13 @@
+```jsx
+<SelectField
+  label="Favourite"
+  helpText="Pick wisely"
+  options={[
+    { label: 'JavaScript', value: 'js' },
+    { label: 'TypeScript', value: 'ts' }
+  ]}
+  size="sm"
+  status="success"
+  required
+/>
+```
