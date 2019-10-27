b1450f649fc2e03f3d8f4ce7758eabfa299e1e77
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-27 12:24:23 +0800

    add array util

---
+++ src/lib/array.spec.ts
@@ -0,0 +1,19 @@
+import { flattenArray } from './array';
+
+test('flattenArray', () => {
+  expect(flattenArray<number>([1, [2, 3, [4], 5], 6, [7]])).toEqual([
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7
+  ]);
+  expect(flattenArray<number>([1, [2], 3])).toEqual([1, 2, 3]);
+  expect(flattenArray([[[1]], { a: 'a' }, [[{ b: 'b' }]]])).toEqual([
+    1,
+    { a: 'a' },
+    { b: 'b' }
+  ]);
+});
---
+++ src/lib/array.ts
@@ -0,0 +1,27 @@
+export interface NestedArray<T> extends Array<NestedArray<T> | T> {}
+
+/**
+ * @see https://github.com/jonschlinkert/arr-flatten
+ * @license MIT
+ */
+export function flattenArray<T>(nestedArray: NestedArray<T>): T[] {
+  const result: T[] = [];
+
+  flatten(nestedArray, result);
+
+  return result;
+}
+
+function flatten<T>(input: NestedArray<T>, result: NestedArray<T>) {
+  let arrLen = input.length;
+  let index = 0;
+
+  while (arrLen--) {
+    const current = input[index++];
+    if (Array.isArray(current)) {
+      flatten(current, result);
+    } else {
+      result.push(current);
+    }
+  }
+}
