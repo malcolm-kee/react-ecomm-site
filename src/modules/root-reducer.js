1d2174832bf44c7e5983d4866e5872b7e0c47f77
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-28 15:41:45 +0800

    refactor product store to rsk

--- src/modules/root-reducer.js
+++ src/modules/root-reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { authReducer } from './auth/auth.slice';
 import { cartReducer } from './cart/cart.slice';
 import { marketingReducer } from './marketing/marketing.slice';
-import { productReducer } from './products/product.reducer';
+import { productReducer } from './products/product.slice';
 
 export const rootReducer = combineReducers({
   auth: authReducer,
