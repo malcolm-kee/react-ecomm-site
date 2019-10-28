35c42c4724ded65f5bf5ce945e0221c6aa231b09
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-28 14:51:56 +0800

    refactor auth to use redux-starter-kit

--- src/modules/auth/auth.actions.js
+++ src/modules/auth/auth.actions.js
@@ -1,33 +1,15 @@
 import { save, load, clear } from '../../lib/storage';
-import * as actionKeys from './auth.action-keys';
 import * as authService from './auth.service';
-
-export const login = user => ({
-  type: actionKeys.LOGIN,
-  payload: user
-});
-
-export const authenticating = () => ({
-  type: actionKeys.AUTHENTICATING
-});
-
-export const logout = () => ({
-  type: actionKeys.LOGOUT
-});
-
-export const authError = error => ({
-  type: actionKeys.AUTH_ERROR,
-  payload: error instanceof Error ? error.message : error
-});
+import { authActions } from './auth.slice';
 
 export const initAuthStatus = () => dispatch => {
   const user = load('user');
 
-  dispatch(user ? login(user) : logout());
+  dispatch(user ? authActions.login(user) : authActions.logout());
 };
 
 export const register = ({ email, name }) => dispatch => {
-  dispatch(authenticating());
+  dispatch(authActions.authenticating());
 
   return authService
     .register({
@@ -35,25 +17,25 @@ export const register = ({ email, name }) => dispatch => {
       name
     })
     .then(user => {
-      dispatch(login(user));
+      dispatch(authActions.login(user));
       save('user', user);
     })
-    .catch(err => dispatch(authError(err)));
+    .catch(err => dispatch(authActions.authError(err)));
 };
 
 export const attemptLogin = email => dispatch => {
-  dispatch(authenticating());
+  dispatch(authActions.authenticating());
 
   return authService
     .login({ email })
     .then(user => {
-      dispatch(login(user));
+      dispatch(authActions.login(user));
       save('user', user);
     })
-    .catch(err => dispatch(authError(err)));
+    .catch(err => dispatch(authActions.authError(err)));
 };
 
 export const attemptLogout = () => dispatch => {
   clear('user');
-  dispatch(logout());
+  dispatch(authActions.logout());
 };
---
+++ src/modules/auth/auth.slice.js
@@ -0,0 +1,37 @@
+import { AuthStatus } from './auth.constants';
+import { createSlice } from 'redux-starter-kit';
+
+const DEFAULT_STATE = {
+  status: AuthStatus.Authenticating,
+  user: null,
+  error: ''
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: DEFAULT_STATE,
+  reducers: {
+    authenticating: state => {
+      state.error = '';
+      state.status = AuthStatus.Authenticating;
+    },
+    login: (state, action) => {
+      state.error = '';
+      state.status = AuthStatus.Authenticated;
+      state.user = action.payload;
+    },
+    logout: state => {
+      state.error = '';
+      state.status = AuthStatus.Anonymous;
+      state.user = null;
+    },
+    authError: (state, { payload }) => {
+      state.status = AuthStatus.Anonymous;
+      state.error = payload instanceof Error ? payload.message : payload;
+    }
+  }
+});
+
+export const authReducer = authSlice.reducer;
+
+export const authActions = authSlice.actions;
