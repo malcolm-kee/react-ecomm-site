f2f3d478b6d040e0999104148d0271d53ebf0071
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-28 15:14:02 +0800

    refactor cart to rsk

--- src/modules/cart/cart.actions.js
+++ src/modules/cart/cart.actions.js
@@ -1,32 +1,10 @@
 import { selectProduct } from '../products/product.selectors';
-import * as actionKeys from './cart.action-keys';
-
-export const addItem = (product, qty = 1) => ({
-  type: actionKeys.Add_Item,
-  payload: {
-    product,
-    qty
-  }
-});
-
-export const removeItem = itemIndex => ({
-  type: actionKeys.Remove_Item,
-  payload: itemIndex
-});
-
-export const incrementItemQty = itemIndex => ({
-  type: actionKeys.Increment_Item_Qty,
-  payload: itemIndex
-});
-
-export const decrementItemQty = itemIndex => ({
-  type: actionKeys.Decrement_Item_Qty,
-  payload: itemIndex
-});
-
-export const clearCart = () => ({
-  type: actionKeys.Clear_Cart
-});
+import { cartActions } from './cart.slice';
 
 export const addProductToCart = (productId, qty) => (dispatch, getState) =>
-  dispatch(addItem(selectProduct(getState(), productId), qty));
+  dispatch(
+    cartActions.addItem({
+      product: selectProduct(getState(), productId),
+      qty
+    })
+  );
