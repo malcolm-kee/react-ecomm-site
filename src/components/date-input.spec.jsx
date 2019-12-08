cb0dc4211d902a6c22ba03e5d357e34908b10306
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-12-09 00:06:48 +0800

    add unit test for date input

---
+++ src/components/date-input.spec.jsx
@@ -0,0 +1,45 @@
+import { fireEvent, render } from '@testing-library/react';
+import React from 'react';
+import { DateInput } from './date-input';
+
+describe('<DateInput />', () => {
+  test('able to work without dateFormat', () => {
+    const { getByTestId, getByText } = setup();
+
+    fireEvent.focus(getByTestId('dateinput'));
+    fireEvent.click(getByText('12'));
+
+    expect(getByTestId('dateinput').value.substring(0, 2)).toBe('12');
+  });
+
+  test('able to work with dateFormat', () => {
+    const { getByTestId, getByText } = setup({
+      defaultValue: '2020-01-01',
+      dateFormat: 'yyyy-mm-dd',
+    });
+
+    fireEvent.focus(getByTestId('dateinput'));
+    fireEvent.click(getByText('25'));
+
+    expect(getByTestId('dateinput').value).toBe('2020-01-25');
+  });
+});
+
+function setup({ defaultValue = '', dateFormat } = {}) {
+  function TestBed() {
+    const [value, setValue] = React.useState(defaultValue);
+    return (
+      <DateInput
+        value={value}
+        onChangeValue={setValue}
+        data-testid="dateinput"
+        dateFormat={dateFormat}
+      />
+    );
+  }
+
+  const result = render(<TestBed />);
+  return {
+    ...result,
+  };
+}
