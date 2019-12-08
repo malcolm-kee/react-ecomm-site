3dc31c1f2e5380324c3bb38f2daac5e805464d7d
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-12-08 21:47:02 +0800

    add more test case for date input

--- src/components/date-input.spec.jsx
+++ src/components/date-input.spec.jsx
@@ -3,19 +3,43 @@ import React from 'react';
 import { DateInput } from './date-input';
 
 describe('<DateInput />', () => {
-  test('able to work without dateFormat', async () => {
-    const { getByTestId, getByText } = render(<TestBed />);
+  test('able to work without dateFormat', () => {
+    const { getByTestId, getByText } = setup();
 
     fireEvent.focus(getByTestId('dateinput'));
     fireEvent.click(getByText('12'));
 
     expect(getByTestId('dateinput').value.substring(0, 2)).toBe('12');
   });
+
+  test('able to work with dateFormat', () => {
+    const { getByTestId, getByText } = setup({
+      defaultValue: '2020-01-01',
+      dateFormat: 'yyyy-mm-dd',
+    });
+
+    fireEvent.focus(getByTestId('dateinput'));
+    fireEvent.click(getByText('25'));
+
+    expect(getByTestId('dateinput').value).toBe('2020-01-25');
+  });
 });
 
-function TestBed() {
-  const [value, setValue] = React.useState('');
-  return (
-    <DateInput value={value} onChangeValue={setValue} data-testid="dateinput" />
-  );
+function setup({ defaultValue = '', dateFormat } = {}) {
+  function TestBed() {
+    const [value, setValue] = React.useState(defaultValue);
+    return (
+      <DateInput
+        value={value}
+        onChangeValue={setValue}
+        data-testid="dateinput"
+        dateFormat={dateFormat}
+      />
+    );
+  }
+
+  const result = render(<TestBed />);
+  return {
+    ...result,
+  };
 }
