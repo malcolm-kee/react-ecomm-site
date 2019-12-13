1c73bc591e4faea7957c6647bc671f758342699f
malcolm-kee <malcolm.keeweesiong@gmail.com>
2019-12-13 16:13:33 +0800

    add useBoolean custom hook

---
+++ src/hooks/use-boolean.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * a simple custom hooks.
+ *
+ * The code is so simple that you better be use the state hook directly.
+ */
+export const useBoolean = initialValue => {
+  const [value, setValue] = React.useState(initialValue);
+  const toggle = React.useCallback(() => {
+    setValue(val => !val);
+  }, []);
+  return [value, toggle, setValue];
+};
