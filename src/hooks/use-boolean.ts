b72f9547a068f25a71c4dbf853668a4f0e474f99
malcolm-kee <malcolm.keeweesiong@gmail.com>
2019-12-13 16:23:51 +0800

    convert useBoolean

--- src/hooks/use-boolean.ts
+++ src/hooks/use-boolean.ts
@@ -1,14 +1,14 @@
-import React from 'react';
+import * as React from 'react';
 
 /**
  * a simple custom hooks.
  *
  * The code is so simple that you better be use the state hook directly.
  */
-export const useBoolean = initialValue => {
+export const useBoolean = (initialValue: boolean | (() => boolean)) => {
   const [value, setValue] = React.useState(initialValue);
   const toggle = React.useCallback(() => {
     setValue(val => !val);
   }, []);
-  return [value, toggle, setValue];
+  return [value, toggle, setValue] as const;
 };
