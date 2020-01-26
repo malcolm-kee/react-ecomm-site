4d6663b3706aed283c11e37eee656718726a18f2
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-01-26 16:16:20 +0800

    add new tests

---
+++ src/modules/support/components/feedback-panel.spec.jsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { toast as toastMock } from 'react-toastify';
+import { user } from '../../../lib/test-util';
+import { FeedbackPanel } from './feedback-panel';
+jest.mock('react-toastify');
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe(`<FeedbackPanel />`, () => {
+  it('feedback success when user click Yes', () => {
+    const { getByText } = render(<FeedbackPanel />);
+    user.click(getByText('Yes'));
+    expect(toastMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('feedback success when user try to click No', () => {
+    const { getByText } = render(<FeedbackPanel />);
+    user.click(getByText('No'));
+    expect(toastMock).toHaveBeenCalledTimes(1);
+  });
+});
