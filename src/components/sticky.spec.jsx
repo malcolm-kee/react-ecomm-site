3c75e6169f6b3272606f9436bda8008696c881ee
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-01-26 18:21:26 +0800

    add component test

---
+++ src/components/sticky.spec.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { fireEvent, render, wait } from '@testing-library/react';
+import { Sticky } from './sticky';
+
+describe(`Sticky`, () => {
+  it(`can mount`, async () => {
+    render(
+      <Sticky offsetTop={30} debounce={10}>
+        <p>Bah bah black shop</p>
+      </Sticky>
+    );
+
+    fireEvent.scroll(window);
+
+    await wait();
+  });
+});
