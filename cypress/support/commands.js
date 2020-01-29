02188edf6e7184d86caae9019ac161a1ccfc3306
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-01-29 16:47:04 +0800

    add chat tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,25 @@
 import '@testing-library/cypress/add-commands';
+import { getRandomEmail } from '../test-helper';
+
+Cypress.Commands.add('createUser', ({ name, email = getRandomEmail() }) => {
+  cy.request({
+    url: 'https://ecomm-db.herokuapp.com/api/users',
+    method: 'POST',
+    body: {
+      name,
+      email,
+      joinedDate: Date.now(),
+    },
+  }).then(response => response.body);
+});
+
+Cypress.Commands.add('connectSocket', ({ url }) => {
+  const ws = new WebSocket(url);
+
+  return new Cypress.Promise((fulfill, reject) => {
+    ws.onopen = function() {
+      fulfill(ws);
+    };
+    ws.onerror = reject;
+  });
+});
