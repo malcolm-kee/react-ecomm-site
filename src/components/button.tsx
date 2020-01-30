6b5006b43b6e17e5e5c3826c272aecace4e004b1
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-01-30 15:23:39 +0800

    change stylesheet from bootstrap to tailwind

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -14,6 +14,22 @@ export type ButtonProps = JSX.IntrinsicElements['button'] & {
   children: React.ReactNode;
 };
 
+const colorClasses: Record<NonNullable<ButtonProps['color']>, string> = {
+  default: 'bg-white text-gray-900 shadow',
+  primary: 'bg-blue-500 text-gray-100 shadow',
+  success: 'bg-green-500 text-gray-100 shadow',
+  info: 'bg-teal-500 text-gray-100 shadow',
+  warning: 'bg-orange-500 text-gray-100 shadow',
+  danger: 'bg-red-500 text-gray-100 shadow',
+  link: 'bg-white text-blue-700',
+};
+
+const sizeClasses: Record<NonNullable<ButtonProps['size']>, string> = {
+  lg: 'text-lg leading-relaxed px-4 py-2',
+  sm: 'text-sm leading-tight px-3 py-2',
+  xs: 'text-xs px-2 py-1',
+};
+
 /**
  * Button is a wrapper of `button` element.
  *
@@ -29,9 +45,9 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <button
         className={cx(
-          'btn',
-          color && `btn-${color}`,
-          size && `btn-${size}`,
+          'rounded',
+          color && colorClasses[color],
+          size ? sizeClasses[size] : 'px-4 py-2',
           className
         )}
         type={type}
