52a57cd9284dd928c801a74262c81f58830f5708
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-02-01 06:18:23 +0800

    non-connected component changes

--- src/modules/cart/components/cart-item.jsx
+++ src/modules/cart/components/cart-item.jsx
@@ -2,61 +2,64 @@ import { Link } from '@reach/router';
 import { inject, observer } from 'mobx-react';
 import React from 'react';
 import { Button } from '../../../components/button';
+import { CloseIcon } from '../../../components/icon/close-icon';
 import { ProductImage } from '../../products/components/product-image';
 
 function CartItem({ cart: { items }, index, onDelete }) {
   const item = items[index];
+
   return (
-    <tr>
-      <td>{index + 1}</td>
-      <td className="col-xs-2 col-lg-1">
-        {item.product.images && (
-          <ProductImage
-            url={item.product.images.thumbStandard}
-            webpUrl={item.product.images.thumbWebp}
-            blurUrl={item.product.images.thumbBlur}
-            alt={item.product.name}
-            width={188}
-            height={188}
-          />
-        )}
-      </td>
-      <td>
-        <Link to={`/product/${item.product.id}`}>{item.product.name}</Link>
-      </td>
-      <td className="text-right">{item.product.price}</td>
-      <td>
-        <Button
-          onClick={item.decrementQty}
-          color="primary"
-          size="sm"
-          disabled={!item.canDecrement}
-          data-testid={`reduce-${item.product.id}`}
-        >
-          -
-        </Button>
-        <span data-testid={`qty-for-${item.product.id}`}>{item.qty}</span>
-        <Button
-          onClick={item.incrementQty}
-          color="primary"
-          size="sm"
-          data-testid={`add-${item.product.id}`}
-        >
-          +
-        </Button>
-      </td>
-      <td className="text-right">{item.totalPrice}</td>
-      <td>
+    <div className="mb-2 pb-2 border-b border-gray-300">
+      <div className="flex justify-between items-center">
+        <Link className="text-blue-700" to={`/product/${item.product.id}`}>
+          #{index + 1} {item.product.name}
+        </Link>
         <Button
           onClick={onDelete}
-          color="danger"
           size="sm"
-          data-testid={`remove-${item.product.id}`}
+          aria-labelledby={`${item.product.id}-remove-label`}
         >
-          Remove
+          <CloseIcon width={36} className="fill-current text-gray-500" />
         </Button>
-      </td>
-    </tr>
+        <span className="sr-only" id={`${item.product.id}-remove-label`}>
+          Remove {item.product.name}
+        </span>
+      </div>
+      <div className="flex">
+        <div className="w-32">
+          {item.product.images && (
+            <ProductImage
+              url={item.product.images.thumbStandard}
+              webpUrl={item.product.images.thumbWebp}
+              blurUrl={item.product.images.thumbBlur}
+              alt={item.product.name}
+              width={188}
+              height={188}
+            />
+          )}
+        </div>
+        <div className="mx-2 flex-1 text-right">
+          <div>
+            <Button
+              onClick={item.decrementQty}
+              color="primary"
+              size="sm"
+              disabled={!item.canDecrement}
+            >
+              -
+            </Button>
+            <span className="mx-1">{item.qty}</span>
+            <Button onClick={item.incrementQty} color="primary" size="sm">
+              +
+            </Button>
+          </div>
+          <div className="mt-3">
+            <span className="mr-1">RM</span>
+            <output className="text-2xl">{item.totalPrice}</output>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/modules/cart/components/cart-table-item.jsx
@@ -0,0 +1,72 @@
+import { Link } from '@reach/router';
+import { inject, observer } from 'mobx-react';
+import React from 'react';
+import { Button } from '../../../components/button';
+import { CloseIcon } from '../../../components/icon/close-icon';
+import { ProductImage } from '../../products/components/product-image';
+
+function CartTableItem({ cart: { items }, index, onDelete }) {
+  const item = items[index];
+
+  return (
+    <tr className="border-b border-gray-300">
+      <td>{index + 1}</td>
+      <td>
+        {item.product.images && (
+          <ProductImage
+            url={item.product.images.thumbStandard}
+            webpUrl={item.product.images.thumbWebp}
+            blurUrl={item.product.images.thumbBlur}
+            alt={item.product.name}
+            width={188}
+            height={188}
+          />
+        )}
+      </td>
+      <td className="px-2">
+        <Link className="text-blue-700" to={`/product/${item.product.id}`}>
+          {item.product.name}
+        </Link>
+      </td>
+      <td className="text-right">{item.product.price}</td>
+      <td className="text-center">
+        <Button
+          onClick={item.decrementQty}
+          color="primary"
+          size="sm"
+          disabled={!item.canDecrement}
+          data-testid={`reduce-${item.product.id}`}
+        >
+          -
+        </Button>
+        <span className="mx-1" data-testid={`qty-for-${item.product.id}`}>
+          {item.qty}
+        </span>
+        <Button
+          onClick={item.incrementQty}
+          color="primary"
+          size="sm"
+          data-testid={`add-${item.product.id}`}
+        >
+          +
+        </Button>
+      </td>
+      <td className="text-right text-xl">{item.totalPrice}</td>
+      <td>
+        <Button
+          onClick={onDelete}
+          size="sm"
+          data-testid={`remove-${item.product.id}`}
+          aria-labelledby={`${item.product.id}-remove-label`}
+        >
+          <CloseIcon width={36} className="fill-current text-gray-500" />
+        </Button>
+        <span className="sr-only" id={`${item.product.id}-remove-label`}>
+          Remove {item.product.name}
+        </span>
+      </td>
+    </tr>
+  );
+}
+
+export default inject('cart')(observer(CartTableItem));
