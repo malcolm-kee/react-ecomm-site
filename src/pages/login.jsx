4bfd25e47dc1fa34b56df0559527c8ac720a177f
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-02-01 06:16:35 +0800

    Page changes

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,10 +4,14 @@ import { LoginForm } from '../modules/auth/components/login-form';
 
 export function Login() {
   return (
-    <div className="container">
+    <div className="max-w-xs p-3 mx-auto">
       <LoginForm />
-      <p>
-        Don't have an account? <Link to="/signup">Signup here</Link>.
+      <p className="py-2">
+        Don't have an account?{' '}
+        <Link className="text-blue-700" to="/signup">
+          Signup here
+        </Link>
+        .
       </p>
     </div>
   );
--- src/pages/not-found-page.jsx
+++ src/pages/not-found-page.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 export function NotFoundPage() {
   return (
-    <div>
-      <div className="container">
-        <h1>Page Not Found</h1>
-        <blockquote>
-          <p>This is probably my fault.</p>
-          <p>I'm sorry, but no free voucher for you.</p>
-        </blockquote>
+    <div className="max-w-lg mx-auto px-3 py-6">
+      <h1 className="text-4xl text-gray-700">Page Not Found</h1>
+      <div className="py-3">
+        <p>This is probably my fault.</p>
+        <p>I'm sorry, but no free voucher for you.</p>
       </div>
     </div>
   );
