94c4bea4549fcb15f4117b00d2e38dcdf3a1c8f4
malcolm-kee <malcolm.keeweesiong@gmail.com>
2020-02-05 17:22:47 +0800

    fix career page mobile view

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -1,13 +1,24 @@
 import * as React from 'react';
-import serverWoman from '../images/server-woman.png';
 import { Input } from '../components/input';
+import serverWoman from '../images/server-woman.png';
 
 export const CareersPage = () => {
   return (
     <div className="max-w-4xl mx-auto">
-      <div className="flex items-center">
-        <div className="flex-1 text-right">
-          <h1 className="text-5xl text-gray-700">Careers in Shopit</h1>
+      <div className="block sm:hidden text-center">
+        <img
+          src={serverWoman}
+          width="300"
+          height="300"
+          alt=""
+          className="mx-auto"
+        />
+      </div>
+      <div className="sm:flex items-center">
+        <div className="flex-1 text-center sm:text-right">
+          <h1 className="text-4xl sm:text-5xl text-gray-700">
+            Careers in Shopit
+          </h1>
           <p>Change the World, Differently</p>
           <div className="inline-block w-56 my-4">
             <Input
@@ -23,7 +34,7 @@ export const CareersPage = () => {
             </datalist>
           </div>
         </div>
-        <div className="flex-1">
+        <div className="hidden sm:block flex-1">
           <img src={serverWoman} width="600" height="600" alt="" />
         </div>
       </div>
