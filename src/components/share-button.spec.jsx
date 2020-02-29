118deda87a243a737566d9ea421126e0049a5324
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-02-29 17:57:14 +0800

    add more tests

---
+++ src/components/share-button.spec.jsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { ShareButton } from './share-button';
+import { user } from '../lib/test-util';
+
+describe(`<ShareButton />`, () => {
+  test(`It copy content and show Copied text for a while`, async () => {
+    const { getByText, queryByText, findByText } = render(
+      <ShareButton urlToShare="https://google.com" />
+    );
+    expect(getByText('Share')).toBeVisible();
+    user.click(getByText('Share'));
+
+    expect(queryByText('Share')).toBeNull();
+    expect(getByText('Link copied!')).toBeVisible();
+
+    const buttonAfterResume = await findByText('Share');
+
+    expect(buttonAfterResume).toBeVisible();
+  });
+});
