8223398f2f0106209d99f6f907c8e3d84241ac89
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-03-01 17:34:41 +0800

    test filling up payment form

--- src/lib/test-util.jsx
+++ src/lib/test-util.jsx
@@ -4,10 +4,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { rootReducer } from '../modules/root-reducer';
 
-export const renderWithStateMgmt = ui => {
+export const renderWithStateMgmt = (ui, { actions = [] } = {}) => {
   const store = configureStore({
     reducer: rootReducer,
   });
+
+  actions.forEach(action => store.dispatch(action));
 
   const renderResult = render(<Provider store={store}>{ui}</Provider>);
 
--- src/modules/cart/components/payment-form.spec.jsx
+++ src/modules/cart/components/payment-form.spec.jsx
@@ -1,7 +1,48 @@
+import { fireEvent } from '@testing-library/react';
 import React from 'react';
 import { renderWithStateMgmt } from '../../../lib/test-util';
+import { cartActions } from '../cart.slice';
 import { PaymentForm } from './payment-form';
 
 test(`PaymentForm can be rendered`, () => {
   renderWithStateMgmt(<PaymentForm />);
+});
+
+test(`PaymentForm can be filled`, () => {
+  const { getByText, getByLabelText } = renderWithStateMgmt(<PaymentForm />, {
+    actions: [
+      cartActions.addItem({
+        product: {
+          id: 1,
+          price: 200,
+        },
+      }),
+    ],
+  });
+
+  expect(getByText('RM 200.00')).not.toBeNull();
+  expect(getByText('Pay').disabled).toBe(true);
+
+  fireEvent.change(getByLabelText('Card Number'), {
+    target: {
+      value: '5572336646354657',
+    },
+  });
+  fireEvent.change(getByLabelText('Name'), {
+    target: {
+      value: 'James Bond',
+    },
+  });
+  fireEvent.change(getByLabelText('Valid Thru'), {
+    target: {
+      value: '12/25',
+    },
+  });
+  fireEvent.change(getByLabelText('CVC'), {
+    target: {
+      value: '123',
+    },
+  });
+
+  expect(getByText('Pay').disabled).toBe(false);
 });
