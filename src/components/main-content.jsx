b1d14da01abcb68d8d2641180358f1c61877fd98
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-03-01 15:14:50 +0800

    add credit card component with style tweak

--- src/components/main-content.jsx
+++ src/components/main-content.jsx
@@ -3,5 +3,8 @@ import React from 'react';
 import styles from './main-content.module.scss';
 
 export const MainContent = props => (
-  <main {...props} className={cx(styles.main, props.className)} />
+  <main
+    {...props}
+    className={cx(styles.main, 'bg-gray-100 pb-4', props.className)}
+  />
 );
--- src/modules/cart/components/payment-form.jsx
+++ src/modules/cart/components/payment-form.jsx
@@ -1,32 +1,119 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Alert } from '../../../components/alert';
 import { Button } from '../../../components/button';
+import { Spinner } from '../../../components/spinner';
 import { TextField } from '../../../components/text-field';
 import { formatMoney } from '../../../lib/format';
 import { selectUser } from '../../auth/auth.selectors';
+import { makePayment } from '../cart.actions';
 import { selectCartTotal } from '../cart.selectors';
 
-const PaymentFormView = ({ defaultName, totalAmount }) => {
-  const [name, setName] = React.useState(defaultName);
+const CreditCard = React.lazy(() => import('../../../components/credit-card'));
 
-  return (
-    <form>
-      <div className="text-center py-4">
-        <div>Total Amount</div>
-        <div className="text-3xl">RM {totalAmount}</div>
+const PaymentFormView = ({ defaultName, totalAmount, pay }) => {
+  const [name, setName] = React.useState(defaultName);
+  const [number, setNumber] = React.useState('');
+  const [cvc, setCvc] = React.useState('');
+  const [expiry, setExpiry] = React.useState('');
+  const [focusedField, setFocusedField] = React.useState(undefined);
+
+  const [paid, setPaid] = React.useState(false);
+
+  return paid ? (
+    <Alert color="success">
+      <p className="text-xl text-center">Paid</p>
+      <div className="text-center py-3">
+        <Link to="/" className="text-blue-500">
+          Back to Home
+        </Link>
       </div>
-      <fieldset>
-        <legend className="pl-2 w-full border-b border-gray-400 font-semibold leading-loose">
-          Billing Details
-        </legend>
-        <TextField label="Name" value={name} onChangeValue={setName} />
-      </fieldset>
-      <div className="text-right pt-4">
-        <Button color="primary" disabled={!name} className="w-full">
-          Pay
-        </Button>
-      </div>
-    </form>
+    </Alert>
+  ) : (
+    <React.Suspense fallback={<Spinner />}>
+      <form
+        onSubmit={ev => {
+          ev.preventDefault();
+          pay(name).then(() => setPaid(true));
+        }}
+      >
+        <div className="text-center py-4">
+          <div>Total Amount</div>
+          <div className="text-3xl">RM {totalAmount}</div>
+        </div>
+        <fieldset>
+          <legend className="pl-2 w-full border-b border-gray-400 font-semibold leading-loose">
+            Billing Details
+          </legend>
+          <div className="sm:flex">
+            <div className="py-2">
+              <CreditCard
+                name={name}
+                number={number}
+                cvc={cvc}
+                expiry={expiry}
+                focused={focusedField}
+              />
+            </div>
+            <div className="flex-1 sm:px-3">
+              <TextField
+                label="Card Number"
+                value={number}
+                onChangeValue={setNumber}
+                onFocus={() => setFocusedField('number')}
+                type="tel"
+                autoComplete="cc-number"
+              />
+              <TextField
+                label="Name"
+                value={name}
+                onChangeValue={setName}
+                onFocus={() => setFocusedField('name')}
+                autoComplete="cc-name"
+              />
+              <div>
+                <div className="w-2/3 inline-block pr-2">
+                  <TextField
+                    label="Valid Thru"
+                    value={expiry}
+                    onChangeValue={newExpiry =>
+                      setExpiry(
+                        newExpiry.length === 2 ? newExpiry + '/' : newExpiry
+                      )
+                    }
+                    placeholder="MM/YY"
+                    onFocus={() => setFocusedField('expiry')}
+                    maxLength={5}
+                    autoComplete="cc-exp"
+                  />
+                </div>
+                <div className="w-1/3 inline-block">
+                  <TextField
+                    label="CVC"
+                    value={cvc}
+                    onChangeValue={setCvc}
+                    onFocus={() => setFocusedField('cvc')}
+                    maxLength={3}
+                    autoComplete="cc-csc"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </fieldset>
+        <div className="text-right pt-4">
+          <Button
+            type="submit"
+            color="primary"
+            disabled={!name || !number || !expiry || !cvc}
+            className="w-full"
+          >
+            Pay
+          </Button>
+        </div>
+      </form>
+    </React.Suspense>
   );
 };
 
@@ -39,4 +126,10 @@ const mapStates = state => {
   };
 };
 
-export const PaymentForm = connect(mapStates)(PaymentFormView);
+const mapDispatch = dispatch => {
+  return {
+    pay: name => dispatch(makePayment({ name })),
+  };
+};
+
+export const PaymentForm = connect(mapStates, mapDispatch)(PaymentFormView);
