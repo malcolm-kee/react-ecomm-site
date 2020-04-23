cfb25494e1f51455088b4515097aea24a1a8fe62
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-04-23 23:53:13 +0800

    fix e2e test

--- cypress/integration/chat.spec.js
+++ cypress/integration/chat.spec.js
@@ -5,49 +5,36 @@ describe(`chat`, () => {
   it(`can launch chat for logged in user`, () => {
     cy.createUser({
       name: 'Malcolm Tee',
-    }).then(user => {
-      cy.visit('/')
-        .findByText('Chat')
-        .click()
+    }).then((user) => {
+      cy.visit('/');
+      cy.findByText('Chat').click();
 
-        .findAllByText('Login')
-        .last()
-        .click()
-        .findByLabelText('Email')
-        .type(user.email)
-        .findAllByText('Login')
-        .last()
-        .click()
+      cy.findAllByText('Login').last().click();
+      cy.findByLabelText('Email').type(user.email);
+      cy.findAllByText('Login').last().click();
 
-        .findByLabelText('Chat message')
-        .type('Hello there!{enter}');
+      cy.findByLabelText('Chat message').type('Hello there!{enter}');
     });
   });
 
   it(`can receives message from other user`, () => {
     cy.createUser({
       name: 'Malcolm Kee',
-    }).then(user => {
+    }).then((user) => {
       cy.createUser({
         name: 'Other People',
-      }).then(otherUser => {
+      }).then((otherUser) => {
         cy.connectSocket({
           url: 'wss://ecomm-db.herokuapp.com/chat',
-        }).then(chatSocket => {
-          cy.visit('/')
-            .findByText('Chat')
-            .click()
+        }).then((chatSocket) => {
+          cy.visit('/');
+          cy.findByText('Chat').click();
 
-            .findAllByText('Login')
-            .last()
-            .click()
-            .findByLabelText('Email')
-            .type(user.email)
-            .findAllByText('Login')
-            .last()
-            .click()
+          cy.findAllByText('Login').last().click();
+          cy.findByLabelText('Email').type(user.email);
+          cy.findAllByText('Login').last().click();
 
-            .findByLabelText('Chat message')
+          cy.findByLabelText('Chat message')
             .type('Hello there!{enter}')
 
             .then(() => {
@@ -63,9 +50,9 @@ describe(`chat`, () => {
                   message: 'I must had said this thousand times',
                 })
               );
-            })
+            });
 
-            .findByText('Hello from the other side')
+          cy.findByText('Hello from the other side')
             .should('be.visible')
             .then(() => {
               chatSocket.close();
@@ -78,21 +65,15 @@ describe(`chat`, () => {
   it(`allow scroll to bottom when there is too much message`, () => {
     cy.createUser({
       name: 'Malcolm Noisy',
-    }).then(user => {
-      cy.visit('/')
-        .findByText('Chat')
-        .click()
+    }).then((user) => {
+      cy.visit('/');
+      cy.findByText('Chat').click();
 
-        .findAllByText('Login')
-        .last()
-        .click()
-        .findByLabelText('Email')
-        .type(user.email)
-        .findAllByText('Login')
-        .last()
-        .click()
+      cy.findAllByText('Login').last().click();
+      cy.findByLabelText('Email').type(user.email);
+      cy.findAllByText('Login').last().click();
 
-        .findByLabelText('Chat message')
+      cy.findByLabelText('Chat message')
         .type('Hello there!{enter}')
         .type('A{enter}')
         .type('B{enter}')
@@ -106,16 +87,13 @@ describe(`chat`, () => {
         .type('J{enter}')
         .type('K{enter}')
         .type('L{enter}')
-        .type('M{enter}')
+        .type('M{enter}');
 
-        .findByTestId('chat-history')
-        .scrollTo('top')
+      cy.findByTestId('chat-history').scrollTo('top');
 
-        .findByText('M')
-        .should('not.be.visible')
+      cy.findByText('M').should('not.be.visible');
 
-        .findByLabelText('Scroll to bottom')
-        .click();
+      cy.findByLabelText('Scroll to bottom').click();
 
       /* not doing this check as it's quite buggy at the moment, 
       refer to https://github.com/cypress-io/cypress/issues/1242 for update */
