c8c67d49143eb63d3cfb693c3b98b484a61b40cf
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-04-30 23:04:45 +0800

    remove all tests

--- src/services/file.service.spec.js
+++
@@ -1,9 +0,0 @@
-import { getFilePreviewUrl, cleanupFilePreviewUrl } from './file.service';
-
-test(`getFilePreviewUrl`, async () => {
-  await getFilePreviewUrl(new File(['abcd', 'eft'], 'file.txt'));
-});
-
-test(`cleanupFilePreviewUrl`, async () => {
-  cleanupFilePreviewUrl('previewUrl');
-});
