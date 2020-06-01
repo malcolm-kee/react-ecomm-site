3831d1a1eb3f593046f8f197550bdf0486cbb8af
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-06-01 22:02:37 +0800

    mock request error

--- cypress/integration/support.spec.js
+++ cypress/integration/support.spec.js
@@ -26,4 +26,27 @@ describe(`support`, () => {
     cy.findByLabelText('Your Phone Number').type(faker.phone.phoneNumber());
     cy.findByText('Submit').click();
   });
+
+  it(`shows error when upload fails`, () => {
+    cy.server();
+    cy.route({
+      url: /upload$/,
+      method: 'POST',
+      status: 500,
+      response: 'Internal Server Error',
+      delay: 200,
+    });
+
+    cy.visit('/help');
+
+    cy.findByText('Complaint').click();
+    cy.findByLabelText('I want to make complain about').select('Product');
+    cy.findByText('Next').click();
+
+    cy.findByLabelText('Upload File').attachFile('html5.png', {
+      subjectType: 'drag-n-drop',
+    });
+
+    cy.findByText('Failed to Upload').should('be.visible');
+  });
 });
