deedeb1e1302be8470fea9c216af30e6e7655413
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-08-01 04:40:04 +0800

    bump deps and fix styleguide (#22)
    * bump deps and fix styleguide
    
    * fix local script

--- src/components/label.tsx
+++ src/components/label.tsx
@@ -9,12 +9,15 @@ import { FieldContext } from './field-context';
  *
  * However, it will sync its `htmlFor` with the id of the input within same `Field` component
  */
-export function Label({ className, ...props }: JSX.IntrinsicElements['label']) {
+export function Label({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'label'>) {
   const { inputId } = React.useContext(FieldContext);
 
   return (
     <label
-      className={cx('pl-2 leading-loose', className)}
+      className={cx('pl-2 text-sm leading-loose text-gray-400', className)}
       htmlFor={inputId}
       {...props}
     />
